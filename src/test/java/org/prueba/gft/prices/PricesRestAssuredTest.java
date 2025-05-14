aa917e117956bfb021c67c8590eff91c50401b94
srioboo <salrio74@gmail.com>
2025-05-14 22:56:34 +0200

    feat: add test required in the subject

--- src/test/java/org/prueba/gft/prices/PricesRestAssuredTest.java
+++ src/test/java/org/prueba/gft/prices/PricesRestAssuredTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.prueba.gft.prices.domain.Prices;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.json.ObjectContent;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -61,8 +63,108 @@ public class PricesRestAssuredTest {
 		assertThat(price.getCurr()).isEqualTo(jsonPrice.getCurr());
 	}
 
+	@DisplayName("14 at 10:00")
 	@Test
-	void shouldGetPriceById() {
-		// TODO - more tests
+	void shouldGetPriceByBrandProductIdAnd1410() {
+		List<Prices> prices = RestAssured.given()
+			.contentType(ContentType.JSON)
+			.when()
+			.get("/prices/brand/1/product/35455?date=2020-06-14-10.00.00")
+			.then()
+			.statusCode(200)
+			.extract()
+			.body()
+			.jsonPath()
+			.getList("$", Prices.class);
+
+		assertThat(prices).isNotEmpty();
+
+		Prices price = prices.getFirst(); // TODO - FILTER CORRECT PRICE
+
+		assertThat(price.getPrice()).isEqualTo(BigDecimal.valueOf(35.50));
+	}
+
+	@DisplayName("14 at 16:00")
+	@Test
+	void shouldGetPriceByBrandProductIdAnd1416() {
+		List<Prices> prices = RestAssured.given()
+			.contentType(ContentType.JSON)
+			.when()
+			.get("/prices/brand/1/product/35455?date=2020-06-14-16.00.00")
+			.then()
+			.statusCode(200)
+			.extract()
+			.body()
+			.jsonPath()
+			.getList("$", Prices.class);
+
+		assertThat(prices).isNotEmpty();
+
+		Prices price = prices.getFirst(); // TODO - FILTER CORRECT PRICE
+
+		assertThat(price.getPrice()).isEqualTo(BigDecimal.valueOf(25.45));
+	}
+
+	@DisplayName("14 at 21:00")
+	@Test
+	void shouldGetPriceByBrandProductIdAnd1421() {
+		List<Prices> prices = RestAssured.given()
+			.contentType(ContentType.JSON)
+			.when()
+			.get("/prices/brand/1/product/35455?date=2020-06-14-21.00.00")
+			.then()
+			.statusCode(200)
+			.extract()
+			.body()
+			.jsonPath()
+			.getList("$", Prices.class);
+
+		assertThat(prices).isNotEmpty();
+
+		Prices price = prices.getFirst(); // TODO - FILTER CORRECT PRICE
+
+		assertThat(price.getPrice()).isEqualTo(BigDecimal.valueOf(35.50));
+	}
+
+	@DisplayName("15 at 10:00")
+	@Test
+	void shouldGetPriceByBrandProductIdAnd1510() {
+		List<Prices> prices = RestAssured.given()
+			.contentType(ContentType.JSON)
+			.when()
+			.get("/prices/brand/1/product/35455?date=2020-06-15-10.00.00")
+			.then()
+			.statusCode(200)
+			.extract()
+			.body()
+			.jsonPath()
+			.getList("$", Prices.class);
+
+		assertThat(prices).isNotEmpty();
+
+		Prices price = prices.getFirst(); // TODO - FILTER CORRECT PRICE
+
+		assertThat(price.getPrice()).isEqualTo(BigDecimal.valueOf(30.50));
+	}
+
+	@DisplayName("16 at 21:00")
+	@Test()
+	void shouldGetPriceByBrandProductIdAnd1621() {
+		List<Prices> prices = RestAssured.given()
+			.contentType(ContentType.JSON)
+			.when()
+			.get("/prices/brand/1/product/35455?date=2020-06-16-21.00.00")
+			.then()
+			.statusCode(200)
+			.extract()
+			.body()
+			.jsonPath()
+			.getList("$", Prices.class);
+
+		assertThat(prices).isNotEmpty();
+
+		Prices price = prices.getFirst(); // TODO - FILTER CORRECT PRICE
+
+		assertThat(price.getPrice()).isEqualTo(BigDecimal.valueOf(38.95));
 	}
 }
