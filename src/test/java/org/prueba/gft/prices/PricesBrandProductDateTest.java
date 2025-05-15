50558d04206be392c018d37c8984ff3acb8d0e0f
srioboo <salrio74@gmail.com>
2025-05-15 11:57:49 +0200

    feat: add test for no prices found and others cases

--- src/test/java/org/prueba/gft/prices/PricesBrandProductDateTest.java
+++ src/test/java/org/prueba/gft/prices/PricesBrandProductDateTest.java
@@ -27,7 +27,7 @@ class PricesBrandProductDateTest {
 		RestAssured.port = port;
 	}
 
-	@ParameterizedTest(name = "{index} - at date {3} product {1} brand {0} is {3}")
+	@ParameterizedTest(name = "{index} - at date {2} product {1} brand {0} is {3}")
 	@CsvSource({
 		"'1', '35455', '2020-06-14-10.00.00', 35.50",
 		"'1', '35455', '2020-06-14-16.00.00', 25.45",
@@ -37,6 +37,10 @@ class PricesBrandProductDateTest {
 		"'2', '35454', '2020-06-14-10.00.00', 35.50",
 		"'2', '35454', '2020-06-14-15.00.00', 25.45",
 		"'2', '35454', '2020-06-14-16.00.00', 25.45",
+		"'3', '35458', '2025-09-14-19.00.00', 130.50",
+		"'3', '35458', '2025-03-15-15.00.00', 130.50",
+		"'3', '35459', '2025-03-14-01.00.00', 250.50",
+		"'3', '35459', '2025-03-15-16.01.00', 32.95",
 	})
 	void shouldGetPriceByBrandProductIdAndDate(String brand, String product, String date, BigDecimal price) {
 		String restUrl = "/prices/brand/" + brand + "/product/" + product + "?date=" + date;
@@ -53,4 +57,23 @@ class PricesBrandProductDateTest {
 			.setScale(2, RoundingMode.UNNECESSARY));
 	}
 
+
+	@ParameterizedTest(name = "{index} - there are not prices for {1} at date {2} for brand {0}")
+	@CsvSource({
+		"'3', '10009', '2025-03-15-16.01.00'",
+		"'4', '35459', '2025-03-15-16.01.00'",
+	})
+	void nullResponse(String brand, String product, String date) {
+		String restUrl = "/prices/brand/" + brand + "/product/" + product + "?date=" + date;
+		Prices prices = RestAssured.given()
+			.contentType(ContentType.JSON)
+			.when()
+			.get(restUrl)
+			.then()
+			.statusCode(200)
+			.extract()
+			.as(Prices.class);
+		assertThat(prices).isNotNull();
+		assertThat(prices.getPrice()).isNull();
+	}
 }
