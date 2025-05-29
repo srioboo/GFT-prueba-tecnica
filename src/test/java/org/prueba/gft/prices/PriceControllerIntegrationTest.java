a64b945ca9455e27b1cfa3f0b9dbead6725a62a8
srioboo <salrio74@gmail.com>
2025-05-29 13:02:13 +0200

    feat: add integration test using testcontainers

--- src/test/java/org/prueba/gft/prices/PriceControllerIntegrationTest.java
+++ src/test/java/org/prueba/gft/prices/PriceControllerIntegrationTest.java
@@ -1,29 +1,45 @@
 package org.prueba.gft.prices;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.prueba.gft.prices.adapters.persistence.DBPricesRepository;
 import org.prueba.gft.prices.domain.model.Prices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 
-// @Testcontainers
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class PriceControllerIntegrationTest {
+class PriceControllerIntegrationTest {
 
 	@Container
 	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13.2")
 		.withDatabaseName("testdb")
 		.withUsername("integration")
 		.withPassword("password");
+
+	@DynamicPropertySource
+	static void overrideProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+		registry.add("spring.datasource.driver-class-name", postgreSQLContainer::getDriverClassName);
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+	}
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -35,6 +51,7 @@ public class PriceControllerIntegrationTest {
 
 	@BeforeEach
 	void setUp() {
+
 		prices = Prices.builder()
 			.brandId(1)
 			.productId(55555)
@@ -47,5 +64,10 @@ public class PriceControllerIntegrationTest {
 			.build();
 	}
 
-
+	@Test
+	void shouldFailReturnPriceForValidRequest() throws Exception {
+		mockMvc.perform(get("/prices/brand/1/product/35455")
+				.param("date", "2020-06-14-15.00.00"))
+			.andExpect(status().isNotFound());
+	}
 }
