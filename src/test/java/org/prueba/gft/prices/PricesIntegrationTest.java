33605a9f33dd07ffe2bc005d2665529d161663b5
srioboo <salrio74@gmail.com>
2025-05-30 12:37:20 +0200

    fix: integration test

--- src/test/java/org/prueba/gft/prices/PricesIntegrationTest.java
+++ src/test/java/org/prueba/gft/prices/PricesIntegrationTest.java
@@ -1,17 +1,25 @@
 package org.prueba.gft.prices;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.prueba.gft.prices.adapters.persistence.DBPricesRepository;
+import org.prueba.gft.prices.adapters.persistence.JpaPricesRepository;
+import org.prueba.gft.prices.domain.model.Prices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,22 +27,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("integration")
 class PricesIntegrationTest {
 
-	@Container
-	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13.2")
-		.withDatabaseName("testdb")
-		.withUsername("integration")
-		.withPassword("password");
+	@Autowired
+	private DBPricesRepository dbPricesRepository;
 
-	@DynamicPropertySource
-	static void overrideProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-		registry.add("spring.datasource.driver-class-name", postgreSQLContainer::getDriverClassName);
-		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-	}
+	@Autowired
+	private JpaPricesRepository repository;
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -45,5 +45,23 @@ class PricesIntegrationTest {
 		mockMvc.perform(get("/prices/brand/1/product/35455")
 				.param("date", "2020-06-14-15.00.00"))
 			.andExpect(status().isNotFound());
+	}
+
+	@DisplayName("Test price is correct")
+	@Sql(scripts = {"classpath:data-integration.sql"})
+	@Test
+	void shouldGetPrice() {
+		Optional<Prices> price = dbPricesRepository
+			.findByProductIdAndBrandIdByDate(35455, 1, LocalDateTime.of(2020, 6, 14, 10, 0));
+		assertThat(price).isPresent();
+		price.ifPresent(prices -> assertThat(prices.getPrice()).isEqualTo(BigDecimal.valueOf(35.5)
+			.setScale(2, RoundingMode.UNNECESSARY)));
+	}
+
+	@DisplayName("Test table count is not 0")
+	@Test
+	void shouldHavePrices() {
+		Long count = repository.count();
+		Assertions.assertNotEquals(0, count);
 	}
 }
